ae89b6861e0e03bbe040d9644d90d91c8234d779
Alexander Kramnik <akramnik@drw.com>
2012-09-06 20:46:44 -0500

    messing around

--- stats.py
+++ stats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+import time
 import curses
 import curses.wrapper
 
@@ -7,10 +9,18 @@ procs = ["first", "second", "third", "fourth"]
 
 def main(stdscr):
     stdscr.border(0)
-    for idx, proc in enumerate(procs):
-        stdscr.addstr(idx + 1, 2, proc)
-    stdscr.refresh()
-    stdscr.getch()
+    stdscr.nodelay(1)
+    curses.curs_set(0)
+    start = int(time.time())
+    while True:
+        stdscr.addstr(1, 2, "Elapsed: " + str(int(time.time())))
+        for idx, proc in enumerate(procs):
+            stdscr.addstr(idx + 3, 2, proc)
+        stdscr.refresh()
+        c = stdscr.getch()
+        if c == ord('q'):
+            sys.exit(0)
+        curses.napms(1000)
 
 if __name__ == "__main__":
     curses.wrapper(main)
